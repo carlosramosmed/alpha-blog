486e5bab4d4c3ff421cf7d6d237b14772a17e7e0
carlosramosmed <carlosr9m@gmail.com>
2021-10-15 00:00:42 +0200

    display validation messages after creating an article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,12 +9,18 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    #This will prevent an undefined error when we load the view for the first time.
+    @article = Article.new
   end
 
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
-    @article.save
-    redirect_to @article #redirect_to article_path(@article) <-- this would work too!
+    if @article.save
+      flash[:notice] = "Article was created successfully!" 
+      redirect_to @article #redirect_to article_path(@article) <-- this would work too!
+    else
+      render 'new'
+    end
   end
 
 end
