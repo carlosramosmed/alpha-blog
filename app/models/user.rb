dd43544c183c1adbfd223996075f22c94de32584
Carlos Ramos <carlosr9m@gmail.com>
2022-01-04 02:14:39 +0100

    create users signup page

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :articles
   validates :username, presence: true, 
                        uniqueness: { case_sensitive: false }, 
-                       length: { in: 3..25, too_short: "Username is too short!", too_long: "Username is too long!" }
+                       length: { in: 3..25, too_short: "is too short!", too_long: "is too long!" }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, 
                     uniqueness: { case_sensitive: false }, 
