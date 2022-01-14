e2a4c676e2543496d6406728865129a53a400ac2
carlosramosmed <carlosr9m@gmail.com>
2022-01-14 18:48:49 +0100

    edit and show for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: %i[show edit update]
 
   def new
     @user = User.new
+  end
+
+  def show
   end
 
   def create
@@ -14,7 +18,23 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "Account updated!"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
   private
+
+  def find_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  require 'digest/md5'
+
+  def gravatar_for user
+    email_address = user.email
+    hash = Digest::MD5.hexdigest(email_address)
+    gravatar_for = "https://www.gravatar.com/avatar/#{hash}"
+    image_tag(gravatar_for, alt: user.username)
+  end
 end
