d530f50cc4e3270380e049ca142d0249f8a382ac
Carlos Ramos <carlosr9m@gmail.com>
2022-01-27 00:44:53 +0100

    auth helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,4 @@
 module ApplicationHelper
-  require 'digest/md5'
-
   def gravatar_for (user, options = { size: 80 })
     email_address = user.email
     hash = Digest::MD5.hexdigest(email_address)
@@ -8,4 +6,13 @@ module ApplicationHelper
     gravatar_for = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
     image_tag(gravatar_for, alt: user.username, class: "rounded shadow mx-auto d-block mb-4")
   end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
 end
