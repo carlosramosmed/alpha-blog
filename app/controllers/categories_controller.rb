96ec79a071ed341c27ab815936d83c93c60c1be3
Carlos Ramos <carlosr9m@gmail.com>
2022-01-27 17:59:38 +0100

    category creation

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,12 +1,33 @@
 class CategoriesController < ApplicationController
 
   def new
+    @category = Category.new
   end
 
   def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = 'Category successfully created!'
+      redirect_to @category
+    else
+      render 'new'
+    end
+  end
+
+  def index
   end
 
   def show
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 
 end
